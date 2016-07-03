eebf039793eee091523d0d921ce1df43db9e1dcf
Jeremy M. Reed <reeje76@gmail.com>
2016-07-03 13:37:16 -0600

    Implement translate(...)

--- rna-transcription.js
+++ rna-transcription.js
@@ -3,14 +3,33 @@ var NotImplementedException = require('./exceptions/NotImplementedException');
 var InvalidParameterException = require('./exceptions/InvalidParameterException');
 
 DnaTranscriber.prototype.translate = function(nucleotide) {
-	throw new NotImplementedException();
+	if ( typeof nucleotide !== 'string' ) {
+		throw new InvalidParameterException('This function takes a single string.');
+	}
+
+	if ( nucleotide.length !== 1 ) {
+		throw new InvalidParameterException('Input should be a single character.');
+	}
+
+	switch(nucleotide) {
+		case 'G':
+			return 'C';
+		case 'C':
+			return 'G';
+		case 'T':
+			return 'A';
+		case 'A':
+			return 'U';
+		default:
+			throw new InvalidParameterException('Invalid nucleotide.');
+	}
 }
 
 DnaTranscriber.prototype.toRna = function(dnaStrand) {
 	var result = '',
 		strandLength = 0;
 	if ( typeof dnaStrand !== 'string') {
-		throw new InvalidParameterException('DnaTranscriber.toRna takes a single string.');
+		throw new InvalidParameterException('This function takes a single string.');
 	}
 
 	result = '';
